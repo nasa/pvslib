37a920b4f9498e6a49455a0448817a305a3b48c8
Munoz <cesaramh@gmail.com>
2015-08-07 12:58:44 -0400

    Updated affine_arith [MM]

--- affine_arith/affine.lisp
+++ affine_arith/affine.lisp
@@ -1,4 +1,13 @@
-(defparameter *aa-builtin* '(("+" "ADD") ("-" "SUB") ("*" "MULT") ("^" "POW")))
+;; patch for fomals-sans-usings applied to nil
+(defmethod formals-sans-usings ((n null)) nil )
+
+(defparameter *aa-builtin* '(("+" "ADD") ("-" "SUB") ("*" "MULT") ("^" "POW")
+			     ("TRUE" "BCONST(TRUE)") ("FALSE" "BCONST(FALSE)")
+			     ("NOT" "BNOT") ("AND" "BAND") ("&" "BAND")
+                             ("OR" "BOR") ("IMPLIES" "BIMPLIES") ("=>" "BIMPLIES")
+			     (">" "REL(>)") (">=" "REL(>=)") ("<" "REL(<)") ("<=" "REL(<=)")
+			     ("=" "EQ")
+			     ("##" "BINCLUDES")))
   
 (defparameter *aa-extended* nil)
 
@@ -224,59 +233,16 @@
   "doing affine")
 
 ;--------------------------------------------------------------------------------
-;-- simple branch and bound strategy --------------------------------------------
+;-- Branch and bound strategies (based on generic strategy from interval@arith) -
 ;--------------------------------------------------------------------------------
-;; Form a string representing the box of variables vars
-(defun aa-box (vars)
-  (if vars
-      (let ((intervals (ia-var-intervals vars)))
-	(format nil "(: ~{~a~^,~} :)" (loop for int in intervals
-					    collect int)))
-    "(::)"))
 
-(defstep aff-simple (expr &optional (precision 3) (n 3) (maxdepth 5))
-  (let ((name    (freshname "sia"))
-	(ia-expr (extra-get-expr expr))
-	(ia-estr (expr2str ia-expr))
-	(fms     (mapcar #'(lambda (fn) (extra-get-formula-from-fnum fn)) (extra-get-fnums '-)))
-	(vars    (ia-get-vars-from-expr ia-expr))
-	(ia-vars (extra-get-var-ranges fms vars))
-	(unvars  (ia-find-unbound-vars ia-vars))
-	(msg     (cond (unvars
-			(format nil "Variable~:[~;s~] ~{~a~^, ~} ~:[is~;are~] unbounded."
-				(cdr unvars) unvars (cdr unvars)))
-		       ((null ia-expr)
-			(format nil "Do not understand argument ~a." expr))
-		       ((not (is-number-expr ia-expr))
-			(format nil "Expresion ~a is not a real number expression." ia-expr))))
-	(aa-box    (unless msg (aa-box ia-vars)))
-	(ia-iexpr  (unless msg (aa-affine-expr ia-expr n ia-vars)))
-	(maxdepth  (if (null ia-vars) 0 maxdepth))
-	(ia-eval   (format nil "simple_affine(~a,~a,~a)" maxdepth name aa-box))
-	(ia-lvars  (format nil "list2array(0)((:~{~a~^, ~}:))" ia-vars))
-	(msg       (or msg (when (listp ia-iexpr) (car ia-iexpr)))))
-    (if msg
-	(printf msg)
-      (spread
-       (name-label name ia-iexpr :hide? t)
-       ((try-branch
-	 (eval-expr ia-eval :safe? nil)
-	 ((then (lemma "simple_affine_soundness")
-		(inst? -1)
-		(replaces -2)
-		(beta -1)
-		(expand "sound?" -1)
-		(branch (split -1)
-			((spread (inst -1 ia-lvars)
-				 ((branch (invoke (case "%1 = %2") (! -1 1) ia-estr)
-					  ((then (replaces -1)
-						 (decimalize -1 precision))
-					   (interval-eq__ name 1)
-					   (then (hide -1)(vars-sharp__))))
-				    (vars-in-box__$)))
-			 (eval-formula))))
-	  (skip))
-	 (skip))))))
+; Simple numerical
+
+(defstep aa-simple-numerical (expr &optional (precision 3) (maxdepth 5))
+  (gbandb_simple-numerical__$ expr precision maxdepth
+			   aa-affine-expr
+			   "simple_affine"
+			   "simple_affine_soundness")
   "[Affine] Computes a simple estimation of the minimum and maximum values
 of EXPR using a branch and bound algorithm based on affine arithmetic.
 PRECISION is the number of decimals in the output interval. The parameter N
@@ -287,200 +253,34 @@ MAXDEPTH is a maximum recursion depth for the branch and bound algorithm.
 This strategy is a simplified version of the more elaborated strategy NUMERICAL."
   "Computing estimates to the minmax of expression ~a,~%using affine arithmetic")
 
-;;
-;; aa-affine-expr
-;;
-;; Return a string representing an affine expression of expr, where
-;;   - n      : approximation parameter
-;;   - var    : list of variables
-;;   - subs   : list of substitutions of the general form (<f> (<F> <n>) <arity>)
-;; Output:
-;;   - Expr such that EXISTS(N:Noise):
-;;     eval_ACExpr_Env(expr,vs) ## eval_ac_noise(Eval_ACExpr_Box(expr,box),N)
+; numerical
+
 (defun aa-affine-expr (expr n vars &optional subs)
-  (setq *aa-let-names* nil)
-  (catch '*aa-error*
-    (aa-affine-expr-rec expr n vars
-			(append subs *aa-builtin* *aa-extended*)
-			nil)))
+  (let ((  *ia-builtin* *aa-builtin*)
+	( *ia-extended* *aa-extended*))
+    (ia-interval-expr expr n vars subs)))
 
-(defun aa-error (msg)
-  (throw '*aa-error* (list msg)))
-
-
-(defun aa-affine-expr-rec (expr n vars subs extended &optional localvars)
-  (let ((val (when (or (is-number-type (type expr)) (is-bool-type (type expr)))
-	       (typecheck (extra-add-evalexpr expr)))))
-    (cond ((and val (is-number-type  (type val)))
-	   (format nil "r2E(~a)" val))
-	  ((and val (is-bool-type (type val)))
-	   (format nil "b2B(~a)" val))
-	  ((is-const-decl-expr expr (mapcar #'car subs)) ;; Is a constant, but not a rational one
-	   ;; At this point, imported chain is OK. 
-	   (let ((opl (ia-idsubs? (expr2str expr) subs)))
-	     (if opl
-		 (let ((op (cadr opl)))
-		   (if (listp op)
-		       (format nil "~a(~a)" (car op) (ia-approx-n n (nth 1 op)))
-		     (format nil "~a" op)))
-	       (aa-error (format nil "Don't know how to translate constant ~a" expr)))))
-	  ((is-variable-expr expr)
-	   (let ((vl (when (name-expr? expr)
-		       (member (id expr) localvars :test #'(lambda(x y) (equal x (car y)))))))
-	     (if vl (ia-format-local-var vl (length vars))
-	       (let ((vl (member (expr2str expr) vars :test #'string=)))
-		 (if vl
-		     (format nil "X(~a)" (- (length vars) (length vl)))
-		   (aa-error (format nil "Don't know how to translate variable ~a" expr)))))))
-	  ((and (unary-application? expr)
-		(is-function-expr expr "-"))
-	   (format nil "NEG(~a)"
-		   (aa-affine-expr-rec (args1 expr) n vars subs extended localvars)))
-	  ((is-function-expr expr "^")
-	   (cond  ;; Simple version of Power (integer power applyed on a variable)
-	          ((and (is-variable-expr (args1 expr)) (is-number-type (type (args2 expr))))
-		   (let*((pow  (args2 expr))
-			 (expr (args1 expr))
-			 (vl   (when (name-expr? expr)
-				 (member (id expr) localvars :test #'(lambda(x y) (equal x (car y)))))))
-		     (if vl (ia-format-local-var vl (length vars))
-		       (let ((vl (member (expr2str expr) vars :test #'string=)))
-			 (if vl
-			     (format nil "POWVAR(~a,~a)"
-				     (- (length vars) (length vl)) pow)
-			   (aa-error (format nil "Don't know how to translate ~a as a simple exponentation" expr)))))))
-		  ;; General power operation.
-		  (t (aa-error (format nil "Unsupported operation: ~a" expr)))))
-	  ((arg-tuple-expr? expr)
-	   (format nil "~{~a~^,~}"
-	  	   (mapcar #'(lambda(x)(aa-affine-expr-rec x n vars subs extended localvars))
-	  		   (exprs expr))))
-	  ((is-function-expr expr)
-	   (let ((opl (ia-idsubs? (id (operator expr)) subs)))
-	     (if opl 
-	  	 (let ((op (cadr opl)))
-	  	   (if (listp op)
-	  	       (format nil "~a(~a)(~a)"
-	  		       (car op) (ia-approx-n n (nth 1 op))
-	  		       (aa-affine-expr-rec (args1 expr) n vars subs extended localvars))
-	  	     (format nil "~a(~a)" op
-	  		     (aa-affine-expr-rec (argument expr) n vars subs extended localvars))))
-	       (aa-error (format nil "Don't know how to translate function ~a" (id (operator expr)))))))
-	  (t (aa-error (format nil "Don't know how to translate expression ~a" expr))))))
-
-
-;--------------------------------------------------------------------------------
-;-- Numerical branch and bound strategy -----------------------------------------
-;--------------------------------------------------------------------------------
-(defstep aff-numerical (expr &optional (precision 3) (maxdepth 10)
+(defstep aa-numerical (expr &optional (precision 3) (maxdepth 10)
 			 min? max?
 			 vars 
+			 subs
 			 dirvar
 			 verbose?
 			 label
 			 (equiv? t))
-  (let ((subs      nil)
-	(name      (freshname "nml"))
-	(label     (or label (freshlabel name)))
-	(accuracy  (ratio2decimal (expt 10 (- precision)) nil precision))
-	(ia-expr   (typecheck (extra-get-expr expr)))
-	(ia-estr   (expr2str ia-expr))
-	(fms       (append (mapcar #'(lambda (f) (extra-get-formula-from-fnum f))
-				   (extra-get-fnums '-))
-			   (mapcar #'(lambda (f) 
-				       (make-negation
-					(extra-get-formula-from-fnum f)))
-				   (extra-get-fnums '+))))
-	(vars      (ia-complete-vars (enlist-it vars) (ia-get-vars-from-expr ia-expr subs)))
-	(initeqs   (extra-reset-evalexprs))
-	(ia-vars   (extra-get-var-ranges fms vars))
-	(unvars    (ia-find-unbound-vars ia-vars))
-	(msg       (cond (unvars
-			  (format nil "Variable~:[~;s~] ~{~a~^,~} ~:[is~;are~] unbounded."
-				  (cdr unvars) unvars (cdr unvars)))
-			 ((null ia-expr)
-			  (format nil "Do not understand argument ~a." expr))
-			 ((not (is-number-type (type ia-expr)))
-			  (format nil "Expresion ~a is not a real number expression." ia-expr))))
-	(ia-box    (unless msg (ia-box ia-vars)))
-	(m_or_m    (+ (if min? -1 0) (if max? 1 0)))
-	(ia-iexpr  (unless msg (aa-affine-expr ia-expr precision ia-vars subs)))
-	(names     (unless msg (append (mapcar #'car *aa-let-names*) (list name))))
-	(exprs     (unless msg (append (mapcar #'cdr *aa-let-names*) (list ia-iexpr))))
-	(namexprs  (merge-lists names exprs))
-	(ia-dirvar (or dirvar (cond ((< m_or_m 0) "mindir_maxvar")
-				    ((> m_or_m 0) "maxdir_maxvar")
-				    (t "altdir_maxvar"))))
-	(maxdepth  (if (null ia-vars) 0 maxdepth))
-	(ia-eval   (format nil "numerical(~a,~a,~a,~a)(~a,~a)"
-			   maxdepth accuracy ia-dirvar m_or_m name ia-box))
-	(ia-lvars  (format nil "list2array(0)((:~{~a~^, ~}:))" ia-vars))
-	(msg       (or msg (when (listp ia-iexpr) (car ia-iexpr)))))
-    (if msg
-	(printf msg)
-      (spread
-       (name-label* namexprs :hide? t)
-       ((try-branch
-	 (eval-expr ia-eval :safe? nil)
-	 ((let ((output (args2 (extra-get-formula -1)))
-		(depth  (extra-get-number-from-expr (get-expr-from-obj output 'depth)))
-		(splits (get-expr-from-obj output 'splits))
-                (ans    (args1 (get-expr-from-obj output 'ans)))
-		(lbacc  (ratio2decimal (- (extra-get-number-from-expr 
-					   (get-expr-from-obj ans 'lb_max))
-					  (extra-get-number-from-expr 
-					   (get-expr-from-obj ans 'mm 'lb)))
-				       true precision))
-		(ubacc  (ratio2decimal (- (extra-get-number-from-expr 
-					   (get-expr-from-obj ans 'mm 'ub))
-					  (extra-get-number-from-expr 
-					   (get-expr-from-obj ans 'ub_min)))
-				       true precision))
-		(eqs    (extra-evalexprs))
-		(maxd   (and ia-vars (= depth maxdepth))))
-	    (with-fresh-labels 
-	     (!iax)
-	     (relabel label -1)
-	     (lemma "numerical_soundness")
-	     (inst? -1)
-	     (replaces label)
-	     (beta -1)
-	     (expand "sound?" -1)
-	     (apply (repeat (expand "length" -1)))
-	     (extra-evalexprs$ eqs !iax)
-	     (branch
-	      (split -1)
-	      ((then
-		(flatten)
-		(relabel label (-2 -3 -4 -5))
-		(hide label)
-		(spread
-		 (inst -1 ia-lvars)
-		 ((branch
-		   (invoke (case "%1 = %2") (! -1 1) ia-estr)
-		   ((then
-		     (when verbose?
-		       (printf "~%----")
-		       (when maxd
-		     	 (printf "Maximum depth has been reached."))
-		       (printf "Lower bound accuracy <= ~a" lbacc) 
-		       (printf "Upper bound accuracy <= ~a" ubacc)
-		       (printf "Splits: ~a. Depth: ~a~%----~%" splits depth)
-		       (printf "See hidden formulas labeled \"~a\" for more information"
-		     	       label))
-		     (replaces -1)
-		     (decimalize -1 precision))
-		    (then (hide -1) (when equiv? (interval-eq__$ names 1 subs)))
-		    (then (hide -1) (reveal !iax) (replaces !iax :hide? nil) (vars-sharp__$))))
-		  (if (null ia-vars)
-		      (eval-formula)
-		    (then 
-		     (reveal !iax)
-		     (replaces !iax :hide? nil) 
-		     (vars-in-box__$))))))
-	       (eval-formula)))))
-	  (skip))
-	 (skip))))))
+  (gbandb_numerical__$ expr precision maxdepth min? max? vars subs dirvar verbose?
+		      label equiv?
+
+		      ;; required-theories:
+		      (("AffineStrategies__")
+		       "This strategy requires importing the theory affine_arith@strategies")
+		      ;; pvsexpr-to-strobj
+		      aa-affine-expr
+		      ;; bandb-function-name
+		      "affine_bandb_numerical.numerical"
+		      ;; bandb soundness theorem name
+		      "affine_bandb_numerical.numerical_soundness"
+		      )
   "[Affine] Computes lower and upper bounds of the minimum and
 maximum values of EXPR using a branch and bound algorithm based on
 affine arithmetic.  PRECISION is the number of decimals in the
@@ -515,7 +315,146 @@ this fact is trivial from a logical point of view, but requires
 unfolding of several definitions which is time consuming in PVS."
   "Computing minmax values of expression ~a,~%via affine arithmetic")
 
+; interval
 
+(defhelper affine-eq__ (names fnum &optional subs nohide)
+  (let ((excl (append
+	       *ia-excluded*
+	       (mapcar #'car *ia-builtin*) 
+	       (mapcar #'car *ia-extended*)
+	       (mapcar #'car subs))))
+    (with-fresh-labels
+     (!ieq fnum)
+     (apply (repeat (expand "length" !ieq)))
+     (apply (repeat (expand names !ieq)))
+     (expand ("X" "r2E" "b2B") !ieq)
+     (apply (repeat (expand "list2array" !ieq)))
+     (apply (repeat (then (expand "beval" !ieq)(expand "realexpr?")(expand "eval" !ieq))))
+     (expand "##")
+     (flatten)
+     (install-rewrites ("real_expr?"
+		       "beval__"
+		       "beval_vs"
+		       "boolexpr_lfcs[ContextData_beval_vs,
+                                      LAMBDA (bexpr:BoolExpr, ctxt0:ContextData_beval_vs):
+                                        LAMBDA (ctxt:ContextData_beval_vs): some?(ctxt`result),
+                                     beval_vs_BCONST, beval_vs_BNOT, beval_vs_BAND, beval_vs_BOR,
+                                     beval_vs_BIMPLIES, beval_vs_BREL, beval_vs_BINCLUDES,
+                                     beval_vs_BITE, beval_vs_BITE_merge1, beval_vs_BITE_merge2,
+                                     beval_vs_BLETIN_RealExpr_ini, beval_vs_BLETIN_RealExpr,
+                                     beval_vs_BLETIN_BoolExpr_merge1, beval_vs_BLETIN_BoolExpr].F"
+		       "beval_vs_BCONST"
+		       "beval_vs_BNOT"
+		       "beval_vs_BAND"
+		       "beval_vs_BOR"
+		       "beval_vs_BIMPLIES"
+		       "beval_vs_BREL"
+		       "beval_vs_BINCLUDES"
+		       "beval_vs_BITE"
+		       "beval_vs_BITE_merge1"
+		       "beval_vs_BITE_merge2"
+		       "beval_vs_BLETIN_RealExpr_ini"
+		       "beval_vs_BLETIN_RealExpr"
+		       "beval_vs_BLETIN_BoolExpr_merge1"
+		       "beval_vs_BLETIN_BoolExpr"))
+     (assert)
+     (assert)
+     (protect (^ !ieq) (then (hide-all-but (!ieq nohide))
+			     (grind :exclude excl)))
+     (assert)))
+  "" "")
+
+(defstep aa-interval (&optional (fnums 1) (precision 3) maxdepth sat?
+			     vars 
+			     ; subs is not supported in this version
+			     dirvar
+			     verbose?
+			     label
+			     (equiv? t)
+			     (tccs? t))
+  
+  (gbandb_interval__$ fnums precision maxdepth sat?
+		      vars 
+		      nil ; subs 
+		      dirvar
+		      verbose?
+		      label
+		      equiv?
+		      tccs? 
+     		      ;; required-theories:
+		      (("AffineStrategies__")
+		       "This strategy requires importing the theory affine_arith@strategies")
+		      ;; pvsexpr-to-strobj
+		      aa-affine-expr
+		      ;; bandb-function-name
+		      "affine_bandb_boolexpr.bandb_boolexpr"
+		      ;; bandb soundness theorem name
+		      "affine_bandb_boolexpr.bandb_boolexpr_soundness"
+		      ("real_expr?"
+		       "beval__"
+		       "beval_vs"
+		       "boolexpr_lfcs[ContextData_beval_vs,
+                                      LAMBDA (bexpr:BoolExpr, ctxt0:ContextData_beval_vs):
+                                        LAMBDA (ctxt:ContextData_beval_vs): some?(ctxt`result),
+                                     beval_vs_BCONST, beval_vs_BNOT, beval_vs_BAND, beval_vs_BOR,
+                                     beval_vs_BIMPLIES, beval_vs_BREL, beval_vs_BINCLUDES,
+                                     beval_vs_BITE, beval_vs_BITE_merge1, beval_vs_BITE_merge2,
+                                     beval_vs_BLETIN_RealExpr_ini, beval_vs_BLETIN_RealExpr,
+                                     beval_vs_BLETIN_BoolExpr_merge1, beval_vs_BLETIN_BoolExpr].F"
+		       "beval_vs_BCONST"
+		       "beval_vs_BNOT"
+		       "beval_vs_BAND"
+		       "beval_vs_BOR"
+		       "beval_vs_BIMPLIES"
+		       "beval_vs_BREL"
+		       "beval_vs_BINCLUDES"
+		       "beval_vs_BITE"
+		       "beval_vs_BITE_merge1"
+		       "beval_vs_BITE_merge2"
+		       "beval_vs_BLETIN_RealExpr_ini"
+		       "beval_vs_BLETIN_RealExpr"
+		       "beval_vs_BLETIN_BoolExpr_merge1"
+		       "beval_vs_BLETIN_BoolExpr")
+		       ;;
+		       affine-eq__$)
+  "[Affine] Checks if formulas FNUMS, which may be simply
+quantified, holds using a branch and bound algorithm based on affine
+arithmetic.  The parameter PRECISION indicates an accuracy of
+10^-PRECISION in every atomic computation. However, this accuracy is
+not guaranteed in the final result. MAXDEPTH is a maximum recursion
+depth for the branch and bound algorithm.
+
+If SAT? is set to t, the strategy checks if formula FNUMS,
+whether in the antecedent or in the consequent, is satisfiable
+and, in the positive case, prints a provably correct witness.
+If formula FNUMS is quantified, it is checked for validity.
+
+VARS is a list of the form (<v1> ... <vn>), where each <vi> is either a
+variable name, e.g., \"x\", or a list consisting of a variable name and
+an interval, e.g., (\"x\" \"[|-1/3,1/3|]\"). This list is used to specify
+the variables in EXPR and to provide their ranges. If this list is not
+provided, this information is extracted from the sequent.
+
+DIRVAR is the name of a direction and variable selection method for
+the branch an bound algorithm. Theory interval_bandb includes some
+pre-defined methods. If none is provided, a choice is made base on the
+problem.
+
+If VERBOSE? is set to t, the strategy prints information about number of
+splits, depth, etc. 
+
+LABEL is used to label formulas containing additional information computed
+by the branch and bound algorithm. These formulas are hidden, but they can
+be brought to the sequent using the proof command REVEAL.
+
+If EQUIV? is set to nil, the strategy doesn't try to prove that the
+deep embedding of the original expression is correct. The proof of
+this fact is trivial from a logical point of view, but requires
+unfolding of several definitions which is time consuming in PVS.
+
+If TCCs? is set to nil, the strategy doesn't try to prove possible
+TCCs generated during its execution."
+  "Checking formula ~a using affine arithmetic")
 
 
 ;--------------------------------------------------------------------------------
@@ -543,7 +482,7 @@ unfolding of several definitions which is time consuming in PVS."
 	    result)
 	(get-sub-expr-at-level (argument expr) (- n 1))))))
 
-(defstep abstract-and-then (fnums level steps)
+(defhelper abstract-and-then (fnums level steps)
   (let ((exprs (get-sub-expr-at-level (extra-get-expr (list '! fnums)) level))
 	(names (freshnames "ABS" (length exprs)))
 	(names-and-exprs nil)
@@ -555,4 +494,3 @@ unfolding of several definitions which is time consuming in PVS."
      (name-replace* names-and-exprs)
      (then steps)))
   "" "")
-	
