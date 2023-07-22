5ef0793248f962aba3bea336422870eabe22cc5b
Cesar Munoz <cesar.a.munoz@nasa.gov>
2023-07-21 23:38:00 -0400

    Fixed .pvs.lisp

--- .pvs.lisp
+++ .pvs.lisp
@@ -1,6 +1,9 @@
-(let ((nasalib-location (extra-pvs-nasalib)))
+;; set *pvs-nasalib-version*
+(defparameter *nasalib-version* "8.0 (mm/dd/yy)")
+
+(let ((nasalib-location (extra-pvs-nasalib))) ;; Returns with a trailing "/"
   (when nasalib-location
-    (load (format nil "~a/RELEASE/nasalib.lisp" nasalib-location))
-    (load (format nil "~a/MetiTarski/metit.lisp" nasalib-location))
-    ;; known sub-libraries
-    (push (format nil "~a/float/" nasalib-location) *pvs-library-path*)))
+    ;; Load lisp code
+    (load (format nil "~aMetiTarski/metit.lisp" nasalib-location))
+    ;; Load known sub-libraries
+    (push (format nil "~afloat/" nasalib-location) *pvs-library-path*)))
