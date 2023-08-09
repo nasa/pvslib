c1d6a02f9512b0d0dfefafd4dbeb15264207b472
Cesar Munoz <cesar.a.munoz@nasa.gov>
2023-08-09 17:41:01 -0400

    Added pvsio-regression

--- pvs-patches/patch-20230712-static-update.lisp
+++ pvs-patches/patch-20230712-static-update.lisp
@@ -1,6 +1,4 @@
 (defmethod contains-possible-closure? ((texpr recordtype))
-  (let ((prtype (print-type texpr)))
-    ;; Strings are internally represented as records, but they are updatable
-    (if (and (type-name? prtype) (equal (id prtype) 'string))
-	(contains-possible-closure? prtype)
-      (contains-possible-closure? (mapcar #'type (fields texpr))))))
+  ;; CM - Strings are internally represented as records, but they are updatable
+  (and (not (tc-eq texpr *string-type*))
+       (contains-possible-closure? (mapcar #'type (fields texpr)))))
