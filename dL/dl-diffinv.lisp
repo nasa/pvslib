0750dcef200b7d849653ace75eb94eae68d2810d
Mariano Moscato <mariano.m.moscato@nasa.gov>
2023-12-15 11:30:51 -0500

    [dL] Fix strategy dl-diffinv

--- dL/dl-diffinv.lisp
+++ dL/dl-diffinv.lisp
@@ -43,6 +43,17 @@
 	       (expand* nnqb_ "normalize")
 	       (for@ nil (expand "normalize_rec"))
 	       (for@ nil (expand "neg_rel"))
+
+	       ;; need to solve the three hypothesis of dl_dI_dlTRUE
+	       ;; 1) derivable_M_nqbool?(max_var(ode),DLTRUE)(nnP) AND
+	       ;; 2) ( cons(Q,Gamma) |- cons(nqb_to_be(nnP),Delta) ) AND
+	       ;; 3) ( Q |- SUB_DIFT(DLTRUE,ode)(nnP) )
+	       (branch
+		(split 1)
+		((else*
+		  (match "derivable_M_nqbool?(%%)(%%)" step (dl-nqboolder__))
+		  (dl-assert)
+		  (dl-subdift__$))))
 	       
 	       ;; (spread
 	       ;; 	(dl-subdift__$)
@@ -51,8 +62,8 @@
 
 	       ;; 20231214 @M3 dl-subdift__ is failing when trying to evaluate max_var( (: ... :) )
 	       ;;              I'll skip it to try and see how to repair it
-	       ;; (dl-subdift__$)
-	       (skip)
+	       (dl-subdift__$)
+	       ;; (skip)
 
 	       )
 	      :side +
@@ -87,7 +98,7 @@ sequent is pretty-printed unless PP? is set to nil."
      ;;  (try-then@
      ;;   ((match "max_var(%)" step (with-tccs (eval-expr "%1" :quiet? t)))
      ;; 	(replaces -1))))
-     (match "derivable_M_nqbool?(%%)(%%)" step (dl-nqboolder__))
+     
      
      (branch
       (then@
@@ -116,7 +127,7 @@ sequent is pretty-printed unless PP? is set to nil."
 	(split 1)
 	((else
 	  (dl-nqboolder__)
-	  (for@ nil (dl-subddt__)))))
+	  (dl-compute-ddt__))))
        (then
 	(hide-all-but 1)
 	(else* (eval-formula 1 :quiet? t)
@@ -128,21 +139,45 @@ sequent is pretty-printed unless PP? is set to nil."
   "Internal strategy. Applies DDL differentiation rules."
   "Applying dl-subdift__ helper")
 
-(defhelper dl-subddt__ ()
-  (let ((expr    (extra-get-expr '(~ + "ddt(%)(%)")))
-	(exprsv '(~ ~ "same_var(%,%)")))
-    (when expr
-      (with-fresh-names
-       ((dt expr :tccs? (eval-formula 1 :quiet? t)))
-       (reveal *dt*)
-       (for@ nil
-	(try-then
-	 ((expand "ddt" *dt*)
-	  (eval-expr exprsv :quiet? t)
-	  (replaces -1)
-	  (replaces *dt* :dir rl)))))))
-  "Internal strategy. Applies ddt differentiaion rules."
-  "Applying dl-subddt__ helper")
+;; @author M3
+;; @notes Use instead of dl-subddt__
+(defhelper dl-compute-ddt__ ()
+  (for@
+   nil
+   (match$
+   "ddt(%a)(dlvar_index(%b))"
+   step (then
+	 (expand "ddt")
+	 (match$
+	  "IF dlvar_index(%c) = dlvar_index(%d) THEN %% ELSE %% ENDIF"
+	  ;; if an IF-THEN-ELSE like this exists in the sequent, is because %c /= %d,
+	  ;; otherwise, the implicit 'assert' applied during the last 'expand' would
+	  ;; have simplified the IF-THEN-ELSE. @M3
+	  step 
+	  (try (typepred "%c")
+	       (replace 1 :hide? t)
+	       (try (typepred "%d") (replace 1 :hide? t) (skip)))
+	       
+	  ))))
+  "Internal stragty. Computes expressions of form ddt((: %% :))(dlvar_index(%))."
+  "Applying dl-compute-ddt__ helper")
+
+;; TODO Remove
+;; (defhelper dl-subddt__ ()
+;;   (let ((expr    (extra-get-expr '(~ + "ddt(%)(%)")))
+;; 	(exprsv '(~ ~ "same_var(%,%)")))
+;;     (when expr
+;;       (with-fresh-names
+;;        ((dt expr :tccs? (eval-formula 1 :quiet? t)))
+;;        (reveal *dt*)
+;;        (for@ nil
+;; 	(try-then
+;; 	 ((expand "ddt" *dt*)
+;; 	  (then
+;; 	   (try (eval-expr exprsv :quiet? t) (replaces -1) (skip))
+;; 	   (replaces *dt* :dir rl))))))))
+;;   "Internal strategy. Applies ddt differentiaion rules."
+;;   "Applying dl-subddt__ helper")
 
 (defhelper dl-nqboolder__ ()
   (let ((drws *dl-domain-rws*) (drre *dl-diffre-rws*))
@@ -159,14 +194,42 @@ sequent is pretty-printed unless PP? is set to nil."
 
 ;; #TODO @M3 downgrade to deflhelper
 (defstep simplify-DIFT_Re-expression ()
-  (let ((dl-operators '(val cnst + - * ^^ ^ sqrt_safe_re div_safe_re))
-	(dl-lemmas    '("dl_dift_val" "dl_dift_const" "dl_dift_plus" ("dl_dift_negative" "dl_dift_minus") "dl_dift_mult" "dl_dift_exp" "dl_dift_pow"
-			;; "dl_dift_div" ;; TODO @M3 this lemma is missing. Deprecated?
+  (then
+   ;; I had to define this tatic to use it with match below, because match
+   ;; was having problems building the final step @M3 20231215
+   (deftactic solve_dlvar_index_le_max_var ()
+     (match$ 1 "dlvar_index(%a) <= max_var(%b)" step
+	     (let ((leq-expr $bj)
+		   (pvs-list (args1 (args2 leq-expr)))
+		   (list (pvslist2list pvs-list))
+		   (idx
+		    (when (car list)
+		      (loop for tuple-expr in (cdr list) for i
+			    from 0 when
+			    (string=
+			     (format
+			      nil
+			      "~a"
+			      (car (exprs tuple-expr)))
+			     "%a")
+			    return i))))
+	       (if idx
+		   (then
+		    (hide-all-but 1)
+		    (lemma "max_var_max")
+		    (branch
+		     (inst -1 "%b" idx)
+		     ((then (simplify_nth)(assert))
+		      (then (for@ nil (expand "length")(assert))))))
+		 (let ((dummy (break "[simplify-DIFT_Re-expression] couldn't find index of %a in %b.")))(skip))))))
+  (let ((dl-operators (list 'val 'cnst '+ '- '* '^^ '^ 'sqrt_safe_re 'div_safe_re))
+	(dl-lemmas    (list "dl_dift_val" "dl_dift_const" "dl_dift_plus" (list "dl_dift_negative" "dl_dift_minus") "dl_dift_mult" "dl_dift_exp" "dl_dift_pow"
 			"dl_dift_sqrt" "dl_dift_divsafe")))
   (match$
    + "DIFT_Re(%1,%2,%3)(%4)"
    step 
-   (let ((lemma-name-per-operator (pairlis dl-operators dl-lemmas))
+   (let ( ;; #+pvsdebug (dummy (break ">>> simplify-DIFT_Re-expression <<<"))
+	 (lemma-name-per-operator (pairlis dl-operators dl-lemmas))
 	 (arg (extra-get-expr "%3")))
      (if (application? arg)
 	 (let((arg-arity (length(arguments arg)))
@@ -181,10 +244,21 @@ sequent is pretty-printed unless PP? is set to nil."
 		    (let ((lemma-name (if (listp lemma-name) (car lemma-name) lemma-name)))
 		      (lemma lemma-name))
 		    ;; (inst -1 diff-bound arg1 domain)
-		    (inst? -1)
-		    (split -1)
-		    (replace -1 :hide? t)
-		    (simplify-DIFT_Re-expression$))
+		    (branch
+		     (inst? -1)
+		     ((branch
+		       (split -1)
+		       ((then
+			 (match$ "derivable_up?(%%)(%%)"
+				 step (let
+					  (#+pvsdebug
+					   (dummy
+					    (format t "~&[simplify-DIFT_Re-expression] Attemting prove-derivable_up-goal~%")))
+				       (prove-derivable_up-goal$)))
+			 (then (replace -1 :hide? t)
+			       (simplify-DIFT_Re-expression$)))))
+		      ;; TCC-branches produced by the instantiation
+		      (solve_dlvar_index_le_max_var))))
 		 (warning "Unexpected unary argument for DIFT_Re application: ~a" arg))
 	     (let ((infix? (infix-application? arg))
 		   (arg2 (format nil "~a" (args2 arg)))
@@ -196,8 +270,13 @@ sequent is pretty-printed unless PP? is set to nil."
 		    (branch
 		     (then@
 		      (inst -1 "%4" arg1 arg2 diff-bound domain)
-		      (split -1)
-		      (replace -1 :hide? t))
+		      (branch
+		       (split -1)
+		       ((then
+			 ;; #+pvsdebug (let ((dummy (break ">2> simplify-DIFT_Re-expression <2<")))(skip))
+			 (match$ "derivable_up?(%%)(%%)"
+				step (prove-derivable_up-goal$))
+			 (replace -1 :hide? t)))))
 		     ((simplify-DIFT_Re-expression$)
 		      (let((drws *dl-domain-rws*)
 			   (drup *dl-derup-rws*))
@@ -218,7 +297,7 @@ sequent is pretty-printed unless PP? is set to nil."
 		(lemma lemma-name)
 		(expand "cnst")
 		(inst?)))
-	   (warning "Unexpected argument for DIFT_Re application: ~a" arg)))))))
+	   (warning "Unexpected argument for DIFT_Re application: ~a" arg))))))))
   "Simplifies  DIFT_Re(%1,%2)(%3)"
   "Applying prove-predicate-on-re helper")
 
